8b12524c3b185345818b5301917e57ba39a465ca
David Adams <davidmichaeladams94@gmail.com>
2021-03-08 17:24:38 -0700

    Created current emp
    and played around with left, inner, and full joins to accomplish a task that helped PH HR employee Bobby accomplish their task.

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -46,3 +46,16 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 -- Select infor from new table
 SELECT * FROM retirement_info;
+
+-- Drop old retirement_info table
+DROP TABLE retirement_info;
+
+-- Create a new table for retiring employees with new rules
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Check the table
+SELECT * FROM retirement_info;
